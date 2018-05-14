cc776533ada40d9adc0598807e93565f11382d6f
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-05-14 18:39:49 +0200

    feat: Add the insert dummy data function

--- app/src/main/java/com/bwaim/inventoryapp/MainActivity.java
+++ app/src/main/java/com/bwaim/inventoryapp/MainActivity.java
@@ -16,16 +16,23 @@
 
 package com.bwaim.inventoryapp;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bwaim.inventoryapp.data.BookStoreContract.BookEntrie;
 import com.bwaim.inventoryapp.data.BookStoreDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private BookStoreDbHelper mDbHelper;
 
@@ -93,5 +100,90 @@ public class MainActivity extends AppCompatActivity {
         } finally {
             c.close();
         }
+    }
+
+    private void insertDummyData() {
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(BookEntrie.COLUMN_PRODUCT_NAME, "BookName");
+        values.put(BookEntrie.COLUMN_PRICE, 10);
+        values.put(BookEntrie.COLUMN_QUANTITY, 5);
+        values.put(BookEntrie.COLUMN_SUPPLIER_NAME, "The Supplier!");
+        values.put(BookEntrie.COLUMN_SUPPLIER_PHONE_NUMBER, "1526545");
+
+        long newRowId = db.insert(BookEntrie.TABLE_NAME, null, values);
+
+        if (newRowId != -1) {
+            Toast.makeText(this, R.string.new_book_inserted, Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "New Book inserted: " + newRowId);
+        } else {
+            Toast.makeText(this, R.string.problem_creating_book
+                    , Toast.LENGTH_SHORT).show();
+            Log.d(LOG_TAG, "Problem when creating the new book.");
+        }
+    }
+
+    /**
+     * Initialize the contents of the Activity's standard options menu.  You
+     * should place your menu items in to <var>menu</var>.
+     * <p>
+     * <p>This is only called once, the first time the options menu is
+     * displayed.  To update the menu every time it is displayed, see
+     * {@link #onPrepareOptionsMenu}.
+     * <p>
+     * <p>The default implementation populates the menu with standard system
+     * menu items.  These are placed in the {@link Menu#CATEGORY_SYSTEM} group so that
+     * they will be correctly ordered with application-defined menu items.
+     * Deriving classes should always call through to the base implementation.
+     * <p>
+     * <p>You can safely hold on to <var>menu</var> (and any items created
+     * from it), making modifications to it as desired, until the next
+     * time onCreateOptionsMenu() is called.
+     * <p>
+     * <p>When you add items to the menu, you can implement the Activity's
+     * {@link #onOptionsItemSelected} method to handle them there.
+     *
+     * @param menu The options menu in which you place your items.
+     * @return You must return true for the menu to be displayed;
+     * if you return false it will not be shown.
+     * @see #onPrepareOptionsMenu
+     * @see #onOptionsItemSelected
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.
+     * The default implementation simply returns false to have the normal
+     * processing happen (calling the item's Runnable or sending a message to
+     * its Handler as appropriate).  You can use this method for any items
+     * for which you would like to do processing without those other
+     * facilities.
+     * <p>
+     * <p>Derived classes should call through to the base class for it to
+     * perform the default menu handling.</p>
+     *
+     * @param item The menu item that was selected.
+     * @return boolean Return false to allow normal menu processing to
+     * proceed, true to consume it here.
+     * @see #onCreateOptionsMenu
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.insert_dummy_data:
+                insertDummyData();
+                displayDatabase();
+                return true;
+            case R.id.delete_all:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
