d42081f45cc270343769c7249aaa96feda05b7cd
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-05-14 17:40:58 +0200

    feat: Add the DbHelper

---
+++ app/src/main/java/com/bwaim/inventoryapp/data/BookStoreDbHelper.java
@@ -0,0 +1,87 @@
+/*
+ *    Copyright 2018 Fabien Boismoreau
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.bwaim.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.bwaim.inventoryapp.data.BookStoreContract.BookEntrie;
+
+/**
+ * Created by Fabien Boismoreau on 14/05/2018.
+ * <p>
+ */
+public class BookStoreDbHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "BookStore.db";
+    private static final String CREATE_DATABASE = "CREATE TABLE " + BookEntrie.TABLE_NAME + " ("
+            + BookEntrie._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + BookEntrie.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+            + BookEntrie.COLUMN_QUANTITY + " INTEGER NOT NULL DEFAULT 0, "
+            + BookEntrie.COLUMN_SUPPLIER_NAME + " TEXT NOT NULL, "
+            + BookEntrie.COLUMN_SUPPLIER_PHONE_NUMBER + " TEXT);";
+
+    /**
+     * Create a helper object to create, open, and/or manage a database.
+     * This method always returns very quickly.  The database is not actually
+     * created or opened until one of {@link #getWritableDatabase} or
+     * {@link #getReadableDatabase} is called.
+     *
+     * @param context to use to open or create the database
+     */
+    public BookStoreDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * Called when the database is created for the first time. This is where the
+     * creation of tables and the initial population of the tables should happen.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_DATABASE);
+    }
+
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     * <p>
+     * <p>
+     * The SQLite ALTER TABLE documentation can be found
+     * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+     * you can use ALTER TABLE to rename the old table, then create the new table and then
+     * populate the new table with the contents of the old table.
+     * </p><p>
+     * This method executes within a transaction.  If an exception is thrown, all changes
+     * will automatically be rolled back.
+     * </p>
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Nothing to do for now
+    }
+}
