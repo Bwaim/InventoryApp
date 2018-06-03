6b4d6196ff44cb5f8e5bc08015f27a2642700781
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-06-03 12:09:14 +0200

    feat: Add Uri information to the contract

--- app/src/main/java/com/bwaim/inventoryapp/data/BookStoreContract.java
+++ app/src/main/java/com/bwaim/inventoryapp/data/BookStoreContract.java
@@ -16,6 +16,8 @@
 
 package com.bwaim.inventoryapp.data;
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -24,10 +26,24 @@ import android.provider.BaseColumns;
  */
 public final class BookStoreContract {
 
+    public static final String CONTENT_AUTHORITY = "com.bwaim.inventoryapp";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_BOOKS = "books";
+
     // To prevent instantiation
     private BookStoreContract() {}
 
     public static class BookEntrie implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
+
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
 
         public static final String TABLE_NAME = "Books";
 
